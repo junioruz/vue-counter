45ff665f63eeb5ea04a27964580bba5d3e443a40
junioruz <umrbekboybayev@gmail.com>
2023-07-08 20:47:32 +0500

    header component

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="wrapper">
+    <MainHeader/>
+  </div>
+</template>
+
+<script>
+
+import MainHeader from "@/components/MainHeader.vue";
+
+  export default {
+    components: {
+      MainHeader
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/MainHeader.vue
@@ -0,0 +1,30 @@
+<template>
+    <header class="header">
+        <div class="logo">
+            <a href="../App.vue">
+                <img src="@/assets/img/logo.svg" alt="logo">
+            </a>
+            <a class="logo__text" href="../App.vue">UDev-counter</a>
+        </div>
+        <nav class="header__nav">
+            <a href="/">Counter</a>
+            <a href="/">About</a>
+            <a href="/">Login</a>
+            <a href="/">Register</a>
+            <a href="/">Contact</a>
+        </nav>
+    </header>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="css" scoped>
+img {
+    color: #5f7d95;
+    width: 3.5em;
+}
+</style>
